9f27172d46e5125fc210ed00561ddbb3a6897e5a
seo <tjdud11160@gmail.com>
2025-10-17 00:03:58 +0900

    test: add test for removing custom separator

--- src/test/java/calculator/CalculateTest.java
+++ src/test/java/calculator/CalculateTest.java
@@ -2,24 +2,33 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculateTest {
     //3. 특수 구분자 판별
     @Test
-    void isCustomSeparator() {
+    void checkCustomSeparatorTest() {
         //given
         String normal = "12:3,4";
         String custom = "//;\n12;3;4";
         Calculate cal = new Calculate();
-
         //when
         boolean normalResult = cal.checkCustomSeparator(normal);
         boolean customResult = cal.checkCustomSeparator(custom);
-
         //then
         assertFalse(normalResult);
         assertTrue(customResult);
+    }
+
+    //4. 구분자 선언부 제거
+    @Test
+    void removeSeparatorTest() {
+        //given
+        String custom = "//;\n12;3;4";
+        Calculate cal = new Calculate();
+        //when
+        String splitCustomSeparator = cal.removeSeparator(custom);
+        //then
+        assertEquals("12;3;4",  splitCustomSeparator);
     }
 }
