0cc458bdb460ed401bf9b797fa620c90ec77f6fa
seo <tjdud11160@gmail.com>
2025-10-17 21:48:25 +0900

    test: add test for checking valid input and separator characters

--- src/test/java/calculator/CalculateTest.java
+++ src/test/java/calculator/CalculateTest.java
@@ -42,4 +42,42 @@ public class CalculateTest {
         //then
         assertEquals(';', separator);
     }
+    //식별자 외 문자인지 판별
+    @Test
+    void isValidCharacterTest() {
+        //given
+        char normal = ',';
+        char custom = ';';
+        char error = '?';
+        char[] normalSeparator = new char[] {',',':'};
+        char[] customSeparator = new char[] {';'};
+        Calculate cal = new Calculate();
+        //when
+        boolean normalCase = cal.isVaildCharacter(normal, normalSeparator);
+        boolean customCase = cal.isVaildCharacter(custom, customSeparator);
+        boolean errorCase = cal.isVaildCharacter(error, normalSeparator);
+        //then[
+        assertTrue(normalCase);
+        assertTrue(customCase);
+        assertFalse(errorCase);
+    }
+    //6. 입력 유효성 검사 (숫자, 식별자에 포함되는 문자)
+    @Test
+    void isValidInput() {
+        //given
+        String normal = "12:3,4";
+        String custom = "12;3;4";
+        String error = "12?4/5";
+        char[] normalSeparator = new char[] {',',':'};
+        char[] customSeparator = new char[] {';'};
+        Calculate cal = new Calculate();
+        //when
+        boolean normalCase = cal.isValidInput(normal, normalSeparator);
+        boolean customCase = cal.isValidInput(custom,  customSeparator);
+        boolean errorCase = cal.isValidInput(error, normalSeparator);
+        //then
+        assertTrue(normalCase);
+        assertTrue(customCase);
+        assertFalse(errorCase);
+    }
 }
